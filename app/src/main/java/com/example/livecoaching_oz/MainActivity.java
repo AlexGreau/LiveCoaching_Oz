58c2d70af0f2e4e186e85174921c09c0177ff17c
AlexG <alexgreau@live.fr>
2019-08-24 15:22:37 +0800

    first commit

---
+++ app/src/main/java/com/example/livecoaching_oz/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.livecoaching_oz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
